478a9c3a9d514c94876f9fed49d957bd30cb747e
Scott Fontenot <fontenot.scott@gmail.com>
2017-11-29 06:38:58 -0500

    set up express

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var app = express();
 var port = 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
-
+app.use(express.static('views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
    res.send('Hello World');
