7cbdd5c56518483d2c07610c78533628178d246a
Scott Fontenot <fontenot.scott@gmail.com>
2017-11-28 21:18:36 -0500

    express generator

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/', function(req, res){
+   res.send('Hello World');
+});
+
+app.get('/books', function(req, res){
+   res.send('Hello Books');
+});
+
+app.listen(port, function(err){
+   console.log('running server on port ' + port);
+});
+
+
+module.exports = app;
