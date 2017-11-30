bdd33206f3da0c5885c84eade2d2b652bd5413e9
Scott Fontenot <fontenot.scott@gmail.com>
2017-11-29 19:37:08 -0500

    troubleshooting ejs

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('views'));
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-   res.send('Hello World');
+
+app.get('/', function(req, res) {
+    res.render('index', {title: 'Hello from render'});
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function(req, res) {
    res.send('Hello Books');
 });
 
-app.listen(port, function(err){
+app.listen(port, function(err) {
    console.log('running server on port ' + port);
 });
 
