c28fd80200d1b04d57e3323f66595d0de80fcbb3
Scott Fontenot <fontenot.scott@gmail.com>
2017-12-02 07:14:41 -0500

    add book routes and book views

--- app.js
+++ app.js
@@ -7,24 +7,39 @@ var path = require('path');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+    }, {
+    Link: '/Authors',
+    Text: 'Author'
+    }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
-
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './src/views');
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/Books', bookRouter);
 
-app.get('/', function(req, res) {
-    res.render('index', {title: 'Hello from render'});
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
-app.get('/books', function(req, res) {
-   res.send('Hello Books');
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
 });
 
-app.listen(port, function(err) {
-   console.log('running server on port ' + port);
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
-
-
-module.exports = app;
