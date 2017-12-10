379a8b83ae3deedcb8827a351ae002741a210104
Scott Fontenot <fontenot.scott@gmail.com>
2017-12-10 18:00:35 -0500

    added goodreads bookId

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -6,48 +6,56 @@ var books = [
         title: 'Classics for Pleasure',
         genre: 'Non-Fiction',
         author: 'Michael Dirda',
+        bookId: '249203',
         read: false
         },
     {
         title: 'A Confederacy of Dunces',
         genre: 'Fiction',
         author: 'John Kennedy Toole',
+        bookId: '310612',
         read: true
         },
     {
         title: 'Car Talk',
         genre: 'Non-Fiction',
         author: ['Tom Maglioozi', 'Ray Magliozzi'],
+        bookId: '49004',
         read: false
         },
     {
         title: 'Where Wizards Stay Up Late',
         genre: 'Non-Fiction',
         author: ['Katie Hafner', 'Matthew Lynon'],
+        bookId: '281818',
         read: false
         },
     {
         title: 'The Return of the Dancing Master',
         genre: 'Fiction',
         author: 'Henning Mankell',
+        bookId: '39797',
         read: false
         },
     {
         title: 'Steve Jobs',
         genre: 'Biography',
         author: 'Walter Issaacson',
+        bookId: '11084145',
         read: false
         },
     {
         title: 'TED Talks: The Official TED Guide to Public Speaking',
         genre: 'Non-Fiction',
         author: 'CHris Anderson',
+        bookId: '25897871',
         read: false
         },
     {
         title: 'Chronology of Tech History',
         genre: 'Non-Ficton',
         author: 'Tom Merritt',
+        bookId: '16093177',
         read: false
         }
     ];
